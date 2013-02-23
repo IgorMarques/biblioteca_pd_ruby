e2fbc54d5e84cb2e2cb7732952b5e633568593e8
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-02-23 09:39:09 -0300

    Adicionado rascunho dos métodos do servidor

--- server.rb
+++ server.rb
@@ -53,5 +53,35 @@ class Server
     @book_collection << book
     return true
   end
+
+  def validate_login(librarian)
+  end
+
+  def validate_logoff(librarian)
+  end
+
+  def update_amout(amount, book)
+  end
+
+  def validate_loan(book, associate)
+  end
+
+  def validade_devolution(book, associate)
+  end
+
+  def inform_books_associate(associate)
+  end
+
+  def get_backup()
+  end
+
+  def notify_aptitude()
+  end
+
+  def notify_overload()
+  end
+
+  def notify_shutdown()
+  end
 end
 
